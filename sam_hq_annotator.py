13ff94ce60f1cb80ded51289b20e73e5779594dd
Tony Wang <jwan690@UoA.auckland.ac.nz>
2024-10-29 20:31:25 +1300

    update sam_hq_annotator: add editor mode, process multiple images

--- sam_hq_annotator.py
+++ sam_hq_annotator.py
@@ -6,6 +6,8 @@ import sys
 from segment_anything import sam_model_registry, SamPredictor
 import warnings
 import os
+import pprint
+import xmltodict
 warnings.filterwarnings("ignore", category=UserWarning)
 
 
@@ -22,6 +24,8 @@ radius = 10
 drawing = False
 value = 255
 edit_mode_triggered = False
+class_id = 0
+current_class_label = None
 
 
 def show_mask(mask, ax, random_color=False):
@@ -43,7 +47,7 @@ def show_points(coords, labels, ax, marker_size=200):
 
 # on click event
 def on_click(event, x, y, flags, param):
-    global input_points, input_labels, radius, drawing, value, final_mask, image
+    global input_points, input_labels, radius, drawing, value, final_mask, image, current_class_label
     clicked_x_corr = int(np.around(x))
     clicked_y_corr = int(np.around(y))
     
@@ -63,24 +67,24 @@ def on_click(event, x, y, flags, param):
         value = [30, 144, 255]
         final_mask = cv2.circle(final_mask, (x, y), radius, value, -1).astype(np.uint8)
         image_with_mask = cv2.addWeighted(image, 1.0, final_mask, 1.0, 0)
-        cv2.imshow('Marine Science Image', image_with_mask)
+        cv2.imshow(f'Marine Science Image - {current_class_label}', image_with_mask)
     elif event == cv2.EVENT_RBUTTONDOWN and edit_mode_triggered == True:
         drawing = True
         value = [0, 0, 0]
         final_mask = cv2.circle(final_mask, (x, y), radius, value, -1).astype(np.uint8)
         image_with_mask = cv2.addWeighted(image, 1.0, final_mask, 1.0, 0)
-        cv2.imshow('Marine Science Image', image_with_mask)
+        cv2.imshow(f'Marine Science Image - {current_class_label}', image_with_mask)
     elif event == cv2.EVENT_MOUSEMOVE and edit_mode_triggered == True:
         if drawing:
             final_mask = cv2.circle(final_mask, (x, y), radius, value, -1).astype(np.uint8)
             image_with_mask = cv2.addWeighted(image, 1.0, final_mask, 1.0, 0)
-            cv2.imshow('Marine Science Image', image_with_mask)
+            cv2.imshow(f'Marine Science Image - {current_class_label}', image_with_mask)
     elif (event == cv2.EVENT_LBUTTONUP or event == cv2.EVENT_RBUTTONUP) and edit_mode_triggered == True:
         drawing = False
 
 
 def predict_mask(input_point, input_label):
-    global image, sam_initialized, predictor, scores, logits, final_mask
+    global image, sam_initialized, predictor, scores, logits, final_mask, current_class_label
     
     input_point = np.array(input_point)
     input_label = np.array(input_label)
@@ -97,7 +101,7 @@ def predict_mask(input_point, input_label):
     mask_input = logits[np.argmax(scores), :, :]
     
     # running real prediction
-    masks, _, _ = predictor.predict(
+    masks, scores, logits = predictor.predict(
         point_coords=input_point,
         point_labels=input_label,
         mask_input=mask_input[None, :, :],
@@ -111,18 +115,65 @@ def predict_mask(input_point, input_label):
     sam_mask = sam_mask.astype(np.uint8)
     final_mask = sam_mask.copy()  # This is the output from HQ-SAM
     image_with_mask = cv2.addWeighted(image, 1.0, final_mask, 1.0, 0)  # Merge mask with original image to see the result
-    cv2.imshow('Marine Science Image', image_with_mask)
+    cv2.imshow(f'Marine Science Image - {current_class_label}', image_with_mask)
     
+def load_annotations(data_path):
+    with open(os.path.join(data_path, "annotations.xml")) as f:
+        dataset = xmltodict.parse(f.read())
+    return dataset
+
+def extract_data_from_cvat_annotations(data_path):
+    dataset = load_annotations(data_path)
+    number_of_images = len(dataset['annotations']['image'])
+    
+    DATA_DICT = dict()
+    task_id_offset = 386
+    for INDEX in range(number_of_images):
+        annotations = dataset['annotations']['image'][INDEX]
+        classes_list = []
+        image_name = annotations['@name']
+        task_id = int(annotations['@task_id']) - task_id_offset
+        img = cv2.imread(os.path.join(data_path, f"task_{task_id}/data", image_name))
+        mask = np.zeros((img.shape[0],img.shape[1]))
+        try:
+            image_boxes_data = annotations['box']
+        except:
+            image_boxes_data = []
+            
+        # checks if there is any bounding box in the frame; otherwise it returns an empty mask for that frame without any class names
+        frame_data_dict= dict()
+        if len(image_boxes_data) == 0:
+            frame_data_dict.update({
+                "task_id": task_id,
+                "image_name": image_name,
+                "classes": []
+            })
+        else:
+            labels = []
+            for j in range(len(image_boxes_data)):
+                try:
+                    if image_boxes_data[j]['@label'] != 'Rock' and image_boxes_data[j]['@label'] != 'Unknown':
+                        labels.append(image_boxes_data[j]["attribute"]['#text'])
+                except:
+                    pass
+                
+            frame_data_dict.update({
+                "task_id": task_id,
+                "image_name": image_name,
+                "classes": labels
+            })
+        DATA_DICT.update({INDEX : frame_data_dict})
+    return DATA_DICT
 
 
 
 def main():
-    global image, final_mask, edit_mode_triggered, radius, predictor
+    global image, final_mask, edit_mode_triggered, radius, predictor, class_id, current_class_label, input_points, input_labels
     
-    # load image
-    image_name = "Deepwatercove_072"
-    image = cv2.imread(f'images/{image_name}.png')
-    image = image.astype(np.uint8)
+    
+    data_path = "images"
+    dataset_dict = extract_data_from_cvat_annotations(data_path)
+    
     
     
     # Initialize HQ-SAM
@@ -135,47 +186,79 @@ def main():
     sam.to(device=device)
     
     predictor = SamPredictor(sam)
-    predictor.set_image(image)
     print('HQ-SAM initialized!')
     
     
-    # Create and open the opencv window
-    cv2.namedWindow('Marine Science Image', cv2.WINDOW_NORMAL)
-    cv2.setMouseCallback("Marine Science Image", on_click)
-    cv2.resizeWindow("Marine Science Image", 1280, 720)
-    cv2.imshow('Marine Science Image', image)
     
     
-    # keyboard events
-    while True:
-        key = cv2.waitKey(1) & 0xFF  # Wait for a key press
-        if key == 27:  # ESC key to exit
-            print("ESC pressed. Exiting...")
-            break
-        elif key == ord('s'):  # 'r' key to display "Red"
-            print("Saving mask image...")
-            
-            old_value = [30, 144, 255]
-            new_value = [255, 255, 255]
-            temp = np.all(final_mask == old_value, axis=-1)
-            final_mask[temp] = new_value
-            
-            os.makedirs('./segmented_masks/', exist_ok=True)
-            cv2.imwrite(f'./segmented_masks/{image_name}.png', final_mask)
-            print("Mask image saved!")
-        elif key == ord('e'):
-            edit_mode_triggered = True
-            print('Edit Mode On')
+    skip_images = 7
+    for i in range(len(dataset_dict)):
+        task_id = dataset_dict[i]['task_id']
+        image_name = dataset_dict[i]['image_name']
+        unique_class_list = list(set(dataset_dict[i]['classes']))
+        if i <= skip_images:
+            print(f"skipping | image id: {i}, task id: {task_id}, image name: {image_name}")
+            continue
+        print(f"image id: {i}, task id: {task_id}, image name: {image_name}")
         
-        if key == ord("1"):
-            radius = 10
-        if key == ord("2"):
-            radius = 20
-        if key == ord("3"):
-            radius = 30  
+        for class_name in unique_class_list:
+            current_class_label = class_name
+            image = cv2.imread(f'./images/task_{task_id}/data/{image_name}')
+            image = image.astype(np.uint8)
             
-    
-    cv2.destroyAllWindows()
+            predictor.set_image(image)
+            
+            # Create and open the opencv window
+            cv2.namedWindow(f'Marine Science Image - {class_name}', cv2.WINDOW_NORMAL)
+            cv2.setMouseCallback(f"Marine Science Image - {class_name}", on_click)
+            cv2.resizeWindow(f"Marine Science Image - {class_name}", 1280, 720)
+            cv2.imshow(f'Marine Science Image - {class_name}', image)
+            
+
+            # keyboard events
+            while True:
+                key = cv2.waitKey(1) & 0xFF  # Wait for a key press
+                if key == 27:  # ESC key to exit
+                    predictor.reset_image()
+                    input_points = []
+                    input_labels = []
+                    edit_mode_triggered = False
+                    print('Edit Mode Off!')
+                    print("ESC pressed. Exiting...")
+                    break
+                elif key == ord('s'):  # 'r' key to display "Red"
+                    print("Saving mask image...")
+                    saved_mask = final_mask.copy()
+                    
+                    old_value = [30, 144, 255]
+                    new_value = [255, 255, 255]
+                    temp = np.all(saved_mask == old_value, axis=-1)
+                    saved_mask[temp] = new_value
+                    
+                    os.makedirs('./segmented_masks/', exist_ok=True)
+                    cv2.imwrite(f'./segmented_masks/{class_name}_{image_name}', saved_mask)
+                    print("Mask image saved!")
+                elif key == ord('e'):
+                    edit_mode_triggered = True
+                    print('Edit Mode On!')
+                elif key == ord('r'):
+                    predictor.reset_image()
+                    predictor.set_image(image)
+                    input_points = []
+                    input_labels = []
+                    final_mask = None
+                    cv2.imshow(f'Marine Science Image - {class_name}', image)
+                    print("HQ-SAM Reset!")
+                
+                if key == ord("1"):
+                    radius = 10
+                if key == ord("2"):
+                    radius = 20
+                if key == ord("3"):
+                    radius = 30  
+                
+        
+            cv2.destroyAllWindows()
     
 
 
